a9b477f844d8d0e0196d3ec18d4d2dfb9e2abe87
George Paul <georgepaulkp@gmail.com>
2023-04-18 12:28:29 +0530

    adding cointrack to GitHub

---
+++ src/stores/showStore.js
@@ -0,0 +1,31 @@
+import axios from 'axios'
+import { create } from 'zustand'
+
+
+const showStore = create((set) => ({
+    graphData: [],
+    
+    reset: () => {
+      set({graphData:[],data:null})  
+    },
+    fetchData: async (id) => {
+        const [graphRes, dataRes] = await Promise.all([
+            axios.get(`https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=121`),
+            axios.get(`https://api.coingecko.com/api/v3/coins/${id}?localization=false&market_data=true`)
+        ])
+        
+        const graphData = graphRes.data.prices.map(price => {
+            const [timeStamp, p] = price
+            const date =new Date(timeStamp).toLocaleDateString("en-us")
+            return {
+                Date: date,
+                Price: p
+              }
+        })
+        console.log(dataRes);
+        set({graphData,dataRes})
+        
+    }
+}))
+
+export default showStore
